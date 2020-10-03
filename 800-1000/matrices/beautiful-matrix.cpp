3bf996c2c081426b7926238d6c4b60134e708b96
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-10-02 18:09:16 -0700

    Create beautiful-matrix.cpp

---
+++ 800-1000/matrices/beautiful-matrix.cpp
@@ -0,0 +1,20 @@
+// 10/2/2020
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int i, j, a;
+
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            cin >> a;
+
+            if(a==1) {
+                cout << (abs(3-i) + abs(3-j));
+            }
+        }
+    }
+    return 0;
+
+}
